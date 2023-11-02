86f869cc80e337ba87b574aaf61fd1713bb4b357
Jan Hein Hoogstad <yeehaa@offcourse.io>
2023-11-02 01:38:46 +0100

    wip: roundtrip

--- bin/main.ml
+++ bin/main.ml
@@ -4,6 +4,5 @@ let () =
   let offcourse_json = Yojson.Basic.from_file "sample_course.json" in
   let offcourse = Offcourse.of_yojson offcourse_json in
   let offcourse_json = Offcourse.to_yojson offcourse in
-  let offcourse_string = Yojson.Basic.to_string offcourse_json in
-  print_endline offcourse_string
+  Yojson.Basic.to_file "sample_course2.json" offcourse_json
 ;;
